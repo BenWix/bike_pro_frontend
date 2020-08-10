f5e2e16236a76b6c61973ade51adff82ea938d9f
BenWix <benwichser@gmail.com>
2020-08-10 18:29:47 -0400

    Add devtools

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { Provider } from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers/manageCustomerAndBikes'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
