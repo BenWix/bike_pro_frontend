ed18478be04cf12459d6441f6fd3bfc6bc34bdd3
BenWix <benwichser@gmail.com>
2020-08-12 10:01:36 -0400

    eliminate uneccesary reducers

--- src/reducers/bikeReducer.js
+++
@@ -1,9 +0,0 @@
-export default function(state = [], action) {
-    // let idx
-    switch (action.type) {
-        case 'ADD_BIKE':
-           return [...state, action.bike]
-        default: 
-        return state
-    }
-}
--- src/reducers/manageCustomerAndBikes.js
+++ src/reducers/manageCustomerAndBikes.js
@@ -1,10 +1,26 @@
-import {combineReducers} from 'redux'
-import bikeReducer from './bikeReducer'
-import customerReducer from './customerReducer'
+// import {combineReducers} from 'redux'
+// import bikeReducer from './bikeReducer'
+// import customerReducer from './customerReducer'
 
-const rootReducer = combineReducers({
-    customers: customerReducer,
-    bikes: bikeReducer
-})
+// const rootReducer = combineReducers({
+//     customers: customerReducer,
+//     bikes: bikeReducer
+// })
 
-export default rootReducer
+// export default rootReducer
+
+export default function(state = {customers: [], loading: false}, action) {
+    // let idx
+    switch (action.type) {
+        case 'LOADING_CUSTOMERS':
+            return {...state, customers: [...state.customers], loading: true}
+
+        case 'ADD_CUSTOMERS':
+            return {...state, customers: [...action.customers], loading: false}
+
+        case 'ADD_CUSTOMER':
+           return {...state, customers: [...state.customers, action.customer], loading: false}
+        default: 
+            return state
+    }
+}
