2395dee0617951de9ca0c76ded32af13ccd2e97e
BenWix <benwichser@gmail.com>
2020-08-12 15:52:37 -0400

    Add customer name to bike list

--- src/components/BikeList.js
+++ src/components/BikeList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-const BikeList = ({bikes, customer}) => {
+const BikeList = ({bikes, customer_name}) => {
     const renderBikes = bikes.map(bike => 
         <li key={bike.id}>
             <Link key={bike.id} to={`bikes/${bike.id}`}>{bike.name}</Link>
@@ -10,7 +10,7 @@ const BikeList = ({bikes, customer}) => {
 
     return (
         <React.Fragment>
-            <h3>Here are all of customer's bikes.</h3>  
+            <h3>Here are all of {customer_name}'s bikes.</h3>  
             <ul>
                 {renderBikes}    
             </ul>  
