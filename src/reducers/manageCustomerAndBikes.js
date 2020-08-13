55a2f8272e49afeb3502c82300c013165fcd8e0d
BenWix <benwichser@gmail.com>
2020-08-13 15:34:10 -0400

    add delete bike to reducer and format

--- src/reducers/manageCustomerAndBikes.js
+++ src/reducers/manageCustomerAndBikes.js
@@ -11,20 +11,13 @@
 
 export default function(state = {customers: [], bike: {}, loading: false}, action) {
     let idx
+    let cust_id
     switch (action.type) {
         case 'LOADING_CUSTOMERS':
             return {...state, customers: [...state.customers], loading: true}
 
         case 'CUSTOMERS_LOADED':
             return {...state, customers: [...action.customers], loading: false}
-
-        // case 'LOADING_CUSTOMER':
-        //     console.log('Loading customer details')
-        //     return {...state, customers: [...state.customers], loading: true}
-
-        // case 'CUSTOMER_LOADED':
-        //     idx = state.customers.findIndex(customer => customer.id === action.customer.id)
-        //     return {customers: [...state.customers.slice(0,idx), action.customer, ...state.customers.slice(idx+1)], loading: false}
 
         case 'ADD_CUSTOMER':
            return {...state, customers: [...state.customers, action.customer], loading: false}
@@ -40,13 +33,31 @@ export default function(state = {customers: [], bike: {}, loading: false}, actio
 
         case 'ADD_BIKE': 
             idx = state.customers.findIndex(customer => customer.id === action.bike.user_id)
-            return {...state,customers: [...state.customers.slice(0,idx),{...state.customers[idx], bikes: state.customers[idx].bikes.concat(action.bike)}, ...state.customers.slice(idx+1)], loading: false}
+            return {...state,
+                customers: 
+                    [...state.customers.slice(0,idx),
+                        {...state.customers[idx], bikes: state.customers[idx].bikes.concat(action.bike)},
+                         ...state.customers.slice(idx+1)], 
+                loading: false}
+        
+        case 'DELETE_BIKE_STARTED':
+            return {...state, loading: true}
+
+        case 'DELTE_BIKE':
+            idx = state.customers.findIndex(customer => customer.id === action.bike.user_id)
+            return {...state, 
+                    customers: 
+                        [...state.customers.slice(0,idx),
+                            {...state.customers[idx], 
+                                bikes: state.customers[idx].bikes.filter(bike => bike.id !== action.bike.id)},
+                            ...state.customers.slice(idx+1)],
+                    loading: false
+            }
         
         case 'ADD_REPAIR_STARTED':
             return {...state, loading: true}
 
         case 'ADD_REPAIR':
-
             return {...state,bike: {...state.bike, repairs: [...state.bike.repairs, action.repair]}, loading: false}
 
         default: 
