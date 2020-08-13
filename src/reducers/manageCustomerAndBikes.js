061c6e1bf8e8bc3b94c5975a34daac380501df6d
BenWix <benwichser@gmail.com>
2020-08-13 11:15:36 -0400

    formatting

--- src/reducers/manageCustomerAndBikes.js
+++ src/reducers/manageCustomerAndBikes.js
@@ -13,7 +13,6 @@ export default function(state = {customers: [], bike: {}, loading: false}, actio
     let idx
     switch (action.type) {
         case 'LOADING_CUSTOMERS':
-            // console.log('Loading all Customers')
             return {...state, customers: [...state.customers], loading: true}
 
         case 'CUSTOMERS_LOADED':
@@ -34,18 +33,13 @@ export default function(state = {customers: [], bike: {}, loading: false}, actio
             return {...state, loading: true}
 
         case 'BIKE_LOADED':
-            debugger
             return {...state, bike: action.bike, loading: false}
-
-
 
         case 'ADD_BIKE_STARTED': 
             return {...state, customers: [...state.customers], loading: true}
 
         case 'ADD_BIKE': 
-            // console.log('Made it to the reducer')
             idx = state.customers.findIndex(customer => customer.id === action.bike.user_id)
-            // debugger
             return {...state,customers: [...state.customers.slice(0,idx),{...state.customers[idx], bikes: state.customers[idx].bikes.concat(action.bike)}, ...state.customers.slice(idx+1)], loading: false}
             
 
