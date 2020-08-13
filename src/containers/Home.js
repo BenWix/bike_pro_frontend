6faff526b263e22e30a13a19cbd53224fa960001
BenWix <benwichser@gmail.com>
2020-08-13 17:17:05 -0400

    Update Home text

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,16 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <p>Lets get to riding some bikes!</p>
+                <h3>Lets get to riding some bikes!</h3>
+
+                <p>To get started, head over to the customers page. From here
+                    you can find an existing customer, check out their bikes, and 
+                    see if they need any repairs!
+                </p>
+                
+                <p>
+                    Once we know there are no repairs needed, it's time to go ride some bikes!
+                </p>
             </div>
         )
     }
