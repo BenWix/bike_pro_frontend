658c45ef5435b7b0762d32cff317375fb524923a
BenWix <44454993+BenWix@users.noreply.github.com>
2020-08-18 22:34:46 -0400

    update repair list to be table

--- src/components/RepairList.js
+++ src/components/RepairList.js
@@ -8,9 +8,25 @@ const RepairList = ({repairs}) => {
         return (
             <React.Fragment>
                 <h3>Repair Log: </h3>  
-                <ul>
-                    {repairs.map(repair => <li key={repair.id}>{repair.repair_type} - Miles: {repair.miles} - {repair.date}</li>)}    
-                </ul>  
+                <table>
+                    <tr>
+                        <th>Repair Type</th>
+                        <th>Miles</th>
+                        <th>Date</th>
+                        <th>Description</th>
+                    </tr>
+                    {repairs.map(repair => {
+                        // <li key={repair.id}>{repair.repair_type} - Miles: {repair.miles} - {repair.date}</li>
+                        return (
+                            <tr>
+                                <th>{repair.repair_type}</th>
+                                <th>{repair.miles}</th>
+                                <th>{repair.date}</th>
+                                <th>{repair.description}</th>
+                            </tr>
+                        )
+                    })}    
+                </table>  
             </React.Fragment>
         )
     } else {
